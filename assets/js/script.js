031319b0e1f8b17e9f9b2f607557aece1da825a5
khoursaaf <khoursa@gmail.com>
2018-10-11 10:27:07 +0200

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+function khoursa() {
+  var firstNumber = document.getElementById("firstNumber").value;
+      secondNumber = document.getElementById("secondNumber").value;
+      result = firstNumber % secondNumber;
+  alert(result)
+}
